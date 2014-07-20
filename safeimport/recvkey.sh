2df147133de40b606301cbd9d7fc6b2d2b21773d
David Leon Gil <coruus@gmail.com>
2014-07-20 17:41:57 -0400

    openpgp-cfb/ocfb_testvectors.go: Go program to generate testvectors of OpenPGP-CFB mode.
    safeimport/recvkey.sh: Switch deprecated use of "echo" to "printf". Other minor changes.

--- safeimport/recvkey.sh
+++ safeimport/recvkey.sh
@@ -2,13 +2,13 @@
 # A small shell-script to receive keys from a keyserver reasonably safely.
 # Author: David Leon Gil
 # License: CC0, attribution kindly requested
-
-fp="$(echo ${1} | tr -d \ )"&&
+fp="$(printf ${1} | tr -d \ )"&&
 touch ./${fp}.pubring.tmp&&
 touch ./${fp}.secring.tmp&&
 2>&1 gpg --keyserver pool.sks-keyservers.net --keyring ./${fp}.pubring.tmp --secret-keyring ./${fp}.secring.tmp --recv-keys ${fp}|tee -a ./${fp}.recvlog&&
 gpg --keyring ./${fp}.pubring.tmp --export ${fp} > ./${fp}.pubkey&&
 2>&1 gpg --import ./${fp}.pubkey|tee -a ./${fp}.importlog&&
+touch ./${fp}.pubring&&
 2>&1 gpg --keyring ./${fp}.pubring --import ./${fp}.pubkey&&
 tar -czvf ./${fp}_log.tgz ./${fp}.*&&
 rm ./${fp}.*
